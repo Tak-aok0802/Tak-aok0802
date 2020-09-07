7495a616ca766f2a72707e7f6b6dab4de6866476
Tak-aok0802 <58761609+Tak-aok0802@users.noreply.github.com>
2020-09-07 17:39:48 +0900

    initial commit
    ポートフォリオ

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+
+// light box
+lightbox.option({
+      'fadeDaration': 500,
+      'resizeDuration': 400
+})
+
+
+// ヘッダー固定
+//  var navPos = $('#gnav').offset().top;
+//
+//  $(window).scroll(function(){
+//    
+//    if($(window).scrollTop() > navPos){
+//      
+//      $('#gnav').css('position', 'fixed');
+//    }else{
+//      
+//      $('#gnav').css('position', 'static');
+//    }
+//  });
+});
